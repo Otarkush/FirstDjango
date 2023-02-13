ca3ebae8302b7dd123d0437e1060bde9b92ebc2c
Андрей Линьков <103899191+Otarkush@users.noreply.github.com>
2023-02-13 18:20:58 +0300

    url about

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -6,5 +6,10 @@ from MainApp import views
 urlpatterns = [
     path('', views.home),
     path('about', views.about),
+    path('item/<int:id>', views.page_item),
+    path('items', views.items_list),
+    # path('items/<int:year>/', views.year_archive),
+    # path('articles/<int:year>/<int:month>/', views.month_archive),
+    # path('articles/<int:year>/<int:month>/<slug:slug>/', views.article_detail),
 ]
 
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render, HttpResponse
+from django.http import Http404
 
 author = {
     'name': 'Andrew',
     'surname': 'Linkov',
     'email': 'andrelinkov@mail.ru'
 }
-
-
+items = [
+   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+   {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
+   {"id": 7, "name": "Картофель фри" ,"quantity":0},
+   {"id": 8, "name": "Кепка" ,"quantity":124},
+]
 # Create your views here.
 def home(request):
-    return HttpResponse('Main page')
+    text = f'''<h1>"Изучаем django"</h1>
+    <strong>Автор</strong>: <i>{author['name']} {author['surname']}</i>
+    '''
+    return HttpResponse(text)
 
 def about(request):
     text = f'''
@@ -19,3 +28,18 @@ def about(request):
     '''
     return HttpResponse(text)
 
+def page_item(request, id):
+    for item in items:
+        if item['id'] == id:
+            text = f'''
+            Tovar {item['name']}<br>
+            '''
+            return HttpResponse(text)
+    raise Http404(f"There isn't such item # {id}")\
+
+def items_list(request):
+    text = '<ol>'
+    for item in items:
+        text += f"<li>{item['name']}</li>"
+    text += '</ol>'
+    return HttpResponse(text)
